7a1ebdd3cdcb0112d4186579b0494bb6fd7043f5
larcondo <larcondo90@gmail.com>
2023-10-21 14:45:03 -0300

    exercise 9.7

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 // import express = require('express');
 const app = express();
-import { calculateBmi } from './utils';
+import { calculateBmi, calculateExercises } from './utils';
 const PORT = 3003;
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -23,6 +25,27 @@ app.get('/bmi', (req, res) => {
     height,
     bmi: calculateBmi(Number(height), Number(weight))
   });
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { target, daily_exercises } = req.body;
+
+  if (!target || !daily_exercises || daily_exercises.length === 0) return res.status(400).json({ error: 'parameter missing' });
+
+  if (isNaN(Number(target))) return res.status(400).json({ error: 'malformatted parameters' });
+
+  const hours: number[] = daily_exercises.map((h:number) => Number(h)); 
+
+  if (hours.some( h => isNaN(h))) return res.status(400).json({ error: 'malformatted parameters' });
+
+  try {
+    const result = calculateExercises(hours, Number(target));
+    return res.status(200).send(result);
+
+  } catch(error: unknown) {
+    return res.status(500).json({ error: 'Server Error /exercises'});
+  }
 });
 
 app.listen(PORT, () => {
